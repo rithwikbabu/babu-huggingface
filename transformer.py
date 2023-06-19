5e1f8501600b762d01d9aa57be97089ee2c480bc
Rithwik Babu <rithwikbabu2020@gmail.com>
2023-06-18 21:06:10 -0500

    starter code

---
+++ transformer.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+qa_model = pipeline("question-answering")
+question = "Where do I live?"
+context = "My name is Merve and I live in İstanbul."
+
+query = qa_model(question = question, context = context)
+## {'answer': 'İstanbul', 'end': 39, 'score': 0.953, 'start': 31}
+
+print(query)
